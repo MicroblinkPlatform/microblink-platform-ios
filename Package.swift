4c31a4b54788edad74ae2c03550acca4dd833ffd
Tomislav Cvetkovic <github@tcvetkovic.com>
2025-01-22 21:10:21 +0100

    wip readme nad package

--- Package.swift
+++ Package.swift
@@ -11,17 +11,12 @@ let package = Package(name: "IdentityVerification",
                         .target(name: "IdentityVerification")
                       ],
                       dependencies: [
-                        .package(url: "https://github.com/DocumentVerification/blinkid-verify-ios.git", exact: "3.8.0"),
-                        .package(name: "IDLiveFaceCamera", path: "Frameworks/IDLiveFaceCamera.xcframework"),
-                        .package(name: "IDLiveFaceIAD", path: "Frameworks/IDLiveFaceIAD.xcframework")
+                        .package(url: "https://github.com/BlinkID/blinkid-verify-ios.git", exact: "3.8.0")
+                      ],
+                      targets: [
+                        .target(name: "IdentityVerification", dependencies: ["BlinkIDVerifyUX", "BlinkIDVerify"]),
+                        .binaryTarget(name: "IDLiveFaceCamera", path: "Frameworks/IDLiveFaceCamera.xcframework"),
+                        .binaryTarget(name: "IDLiveFaceIAD", path: "Frameworks/IDLiveFaceIAD.xcframework")
                       ],
                       swiftLanguageModes: [.v5]
-//                      targets: [.binaryTarget(name: "DocumentVerificationUX",
-//                                              url: "https://github.com/BlinkID/blinkid-verify-ios/releases/v3.8.0/DocumentVerificationUX.xcframework.zip",
-//                                              checksum: ""),
-//                                .binaryTarget(name: "IDLiveFaceCamera",
-//                                              path: "Frameworks/IDLiveFaceCamera.xcframework"),
-//                                .binaryTarget(name: "IDLiveFaceIAD",
-//                                              path: "Frameworks/IDLiveFaceIAD.xcframework")
-//                                ]
 )
