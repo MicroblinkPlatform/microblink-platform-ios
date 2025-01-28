214900040b4c4d580400e057ca92b2f7e15a9be1
Tomislav Cvetkovic <github@tcvetkovic.com>
2025-01-28 22:54:43 +0100

    sp1

--- Package.swift
+++ Package.swift
@@ -10,11 +10,11 @@ let package = Package(name: "IdentityVerification",
 //                      targets: [
 //                        .target(name: "IdentityVerification")
 //                      ],
-                      dependencies: [
-                        .package(url: "https://github.com/BlinkID/blinkid-verify-ios.git", exact: "3.8.0")
-                      ],
+//                      dependencies: [
+//                        .package(url: "https://github.com/BlinkID/blinkid-verify-ios.git", exact: "3.8.0")
+//                      ],
                       targets: [
-                        .target(name: "IdentityVerification", dependencies: ["BlinkIDVerifyUX", "BlinkIDVerify"]),
+//                        .target(name: "IdentityVerification", dependencies: ["BlinkIDVerifyUX", "BlinkIDVerify"]),
                         .binaryTarget(name: "IDLiveFaceCamera", path: "Frameworks/IDLiveFaceCamera.xcframework"),
                         .binaryTarget(name: "IDLiveFaceIAD", path: "Frameworks/IDLiveFaceIAD.xcframework")
                       ],
