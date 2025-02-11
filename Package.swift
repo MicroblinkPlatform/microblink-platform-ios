7ee7d764016c907ea94c09171b0e5ede71a1f9bd
Tomislav Cvetkovic <github@tcvetkovic.com>
2025-02-11 12:52:21 +0100

    update gitignore, release notes, package and swiftpm

--- Package.swift
+++ Package.swift
@@ -1,22 +1,36 @@
 // swift-tools-version: 6.0
-
 import PackageDescription
 
-let package = Package(name: "MicroblinkPlatform",
-                      platforms: [.iOS(.v16)],
-                      products: [
-                        .library(name: "MicroblinkPlatform", targets: ["MicroblinkPlatform"])
-                      ],
-//                      targets: [
-//                        .target(name: "MicroblinkPlatform")
-//                      ],
-                      dependencies: [
-                        .package(url: "https://github.com/BlinkID/blinkid-verify-ios.git", exact: "3.8.0")
-                      ],
-                      targets: [
-                        .target(name: "MicroblinkPlatform", dependencies: ["BlinkIDVerifyUX", "BlinkIDVerify"]),
-                        .binaryTarget(name: "IDLiveFaceCamera", path: "Frameworks/IDLiveFaceCamera.xcframework"),
-                        .binaryTarget(name: "IDLiveFaceIAD", path: "Frameworks/IDLiveFaceIAD.xcframework")
-                      ],
-                      swiftLanguageModes: [.v5]
+let package = Package(
+    name: "MicroblinkPlatform",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(
+            name: "MicroblinkPlatform",
+            targets: ["MicroblinkPlatform"]),
+        .library(
+            name: "IDLiveFaceCamera",
+            targets: ["IDLiveFaceCamera"]),
+        .library(
+            name: "IDLiveFaceIAD",
+            targets: ["IDLiveFaceIAD"])
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "MicroblinkPlatform",
+            path: "Frameworks/MicroblinkPlatform.xcframework"
+        ),
+        .binaryTarget(
+            name: "IDLiveFaceCamera",
+            path: "Frameworks/IDLiveFaceCamera.xcframework"
+        ),
+        .binaryTarget(
+            name: "IDLiveFaceIAD",
+            path: "Frameworks/IDLiveFaceIAD.xcframework"
+        )
+    ],
+    swiftLanguageModes: [.v5]
 )
