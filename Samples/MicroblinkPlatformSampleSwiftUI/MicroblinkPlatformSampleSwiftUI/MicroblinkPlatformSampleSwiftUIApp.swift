51893ce6bc7aa4e4482b67f56eaa8dd44d43d94a
Tomislav Cvetkovic <github@tcvetkovic.com>
2025-03-27 14:08:01 +0100

    add sample and framework

---
+++ Samples/MicroblinkPlatformSampleSwiftUI/MicroblinkPlatformSampleSwiftUI/MicroblinkPlatformSampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroblinkPlatformSampleSwiftUIApp.swift
+//  MicroblinkPlatformSampleSwiftUI
+//
+//  Created by Tomislav Cvetkovic on 27.03.2025..
+//
+
+import SwiftUI
+
+@main
+struct MicroblinkPlatformSampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
